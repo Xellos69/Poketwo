69867ef8c346ef36a1fed10da8c5694e797537ca
Xellos <87872752+Xellos69@users.noreply.github.com>
2024-02-20 12:18:14 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,20 @@
 import asyncio
 import random
+import string
+import json
 import re
-import requests
-import time
 from discord.ext import commands
-from termcolor import colored
+import os
+import time
+import requests
 
-token = "
+token = "MTEyMDM0MDA4MjI2NDY0NTc5NA.GSGUeJ.nbpOfj_ScSDGsU1P4deBU19z0CQ95CqR2DJK3s"
 ownerid = 822830992426926172
 
 client = commands.Bot(command_prefix='$')
 client.remove_command('help')
 
 captcha = True
-shiny = 0
-legendary = 0
-num_pokemon = 0
-mythical = 0
-pokemon_list = ""
 
 def solve(message):
     hint = [char for i, char in enumerate(message[15:-1]) if char != "\\"]
@@ -30,7 +27,7 @@ def LoadPokemons():
     global pokemon_list
     print('Loading Pokémon list..')
     try:
-        url = 'https://raw.githubusercontent.com/Xellos69/catcher/main/pokemon'
+        url = Files['data/pokemon']
         pokemons = requests.get(url)
         with open('data/pokemon', 'w', encoding='utf8') as PokemonList:
             PokemonList.write(pokemons.text)
@@ -44,17 +41,14 @@ def LoadPokemons():
 @client.event
 async def on_ready():
     LoadPokemons()
-    print(colored(f'Autocatch is connected as {client.user}', 'black', 'on_white'))
+    owner = await client.fetch_user(ownerid)
+    await owner.send("I'm Ready Catch")
 
 @client.event
 async def on_message(message):
     global captcha
-    global shiny
-    global legendary
-    global num_pokemon
-    global mythical
 
-    if message.author.id == ownerid and not message.embeds:
+    if message.author.id == 854233015475109888 and not message.embeds:
         content = message.content.lower().strip()
         if content.count(':') == 1:
             name, percentage = content.split(':')
@@ -80,20 +74,9 @@ async def on_message(message):
                         await asyncio.sleep(random.randint(1, 3))
                         channel = message.channel
                         name = i.lower()
-                        await channel.send(f'<@716390085896962058> c {name}')
+                        await channel.send(f'<@716390085896962058> cAtCh {name}')
 
-        elif 'Congratulations' in content:
-            split = content.split(' ')
-            pokemon = ' '.join(split[7:]).replace('!', '')
-            if 'seem unusual...' in content:
-                shiny += 1
-                print(f'Shiny Pokémon caught! Pokémon: {pokemon}')
-                print(f'Shiny: {shiny} | Legendary: {legendary} | Mythical: {mythical}')
-            else:
-                num_pokemon += 1
-                print(f'Total Pokémon Caught: {num_pokemon} :{pokemon}')
-
-        elif 'That is the wrong pokémon!' in content and captcha:
+        if 'That is the wrong pokémon!' in content and captcha:
             await asyncio.sleep(random.randint(1, 3))
             channel = message.channel
             await channel.send(f'<@716390085896962058> h')
@@ -127,10 +110,9 @@ def CheckForUpdates():
                 with open(file, 'w') as arch:
                     arch.write(response.text)
                     print(f'{file} Reloaded.')
-            else:
-                print(f'Failed to update {file}. Status code: {response.status_code}')
         time.sleep(600)
 
+# Define the Files dictionary
 Files = {
     'data/pokemon': 'https://raw.githubusercontent.com/Xellos69/catcher/main/data/pokemon',
 }
